f55733b2fe707756c23f01acdcb832054f1946eb
kazamok <162867216+kazamok@users.noreply.github.com>
2025-08-13 13:22:20 +0900

    Add files via upload

---
+++ config/playermap_config.php
@@ -0,0 +1,39 @@
+<?php
+// 오픈 시 수정필요
+$server[1] = array(
+    "addr" => "127.0.0.1",
+    "game_port" => "8085",
+);
+
+// 오픈 시 수정필요
+$characters_db[1] = array(
+    'addr'     => '127.0.0.1',
+    'user'     => 'acore',
+    'pass'     => 'acore',
+    'name'     => 'acore_characters',
+);
+
+// 오픈 시 수정필요
+$realm_db = array(
+    'addr'     => '127.0.0.1',
+    'user'     => 'acore',
+    'pass'     => 'acore',
+    'name'     => 'acore_auth',
+);
+
+$server_type = 1;
+$map_time = 15;
+$map_show_time = 1;
+$map_show_status = 1;
+$map_time_to_show_uptime = 10;
+$map_time_to_show_maxonline = 10;
+$map_time_to_show_gmonline = 10;
+$gm_online = 1;
+$map_gm_show_online_only_gmoff = 1;
+$map_gm_show_online_only_gmvisible = 1;
+$map_gm_add_suffix = 1;
+$gm_online_count = 1;
+$map_status_gm_include_all = 1;
+$language = "english";
+$site_encoding = "utf8";
+?>
